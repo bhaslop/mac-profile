bb29aa4b4182e2044354183bfae2f809df869e10
Brian Haslop <bhaslop@arar013976.ml.com>
2017-10-10 14:08:07 -0300

    update java-version function

--- java-version.bash
+++ java-version.bash
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-function j6 {
-	export JAVA_HOME="/Library/Java/JavaVirtualMachines/1.6.0_65-b14-462.jdk/Contents/Home/";
-	echo "JAVA 6 applied to this shell";
+function jdk() {
+	if [ $# -ne 0 ]; then
+		removeFromPath '/System/Library/Frameworks/JavaVM.framework/Home/bin'
+		
+		if [ -n "${JAVA_HOME+x}" ]; then
+			removeFromPath $JAVA_HOME
+		fi
+   
+		export JAVA_HOME=`/usr/libexec/java_home -v 1.$@`
+		export PATH=$JAVA_HOME/bin:$PATH
+	fi
 }
 
-function j7 {
-	export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.7.0_51.jdk/Contents/Home";
-	echo "JAVA 7 applied to this shell";
+function removeFromPath() {
+	export PATH=$(echo $PATH | sed -E -e "s;:$1;;" -e "s;$1:?;;")
 }
